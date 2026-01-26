2ab0ee2ed6ffa5ed19ee751db38cc20edcc5f3f7
Joao Pargana <jmpargana@gmail.com>
2026-01-26 09:02:57 +0000

    fix: add int tests and early returns

--- cmd/gq/main.go
+++ cmd/gq/main.go
@@ -21,12 +21,21 @@ var rootCmd = &cobra.Command{
 	Use:   "gq",
 	Short: "jq-like command-line tool written in go",
 	Long:  `A fast, simple, and expressive way to query and transform JSON data from the command line`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		showVersion, _ := cmd.Flags().GetBool("version")
 		if showVersion {
 			fmt.Printf("Version: %s\n", version)
 			os.Exit(0)
 		}
+
+		if err := requireStdin(); err != nil {
+			return err
+		}
+
+		if len(os.Args) < 2 {
+			return fmt.Errorf("no program provided")
+		}
+
 		r := bufio.NewReader(os.Stdin)
 		tokens := lexer.Lex(os.Args[1])
 		p := parser.NewParser(tokens)
@@ -34,7 +43,23 @@ var rootCmd = &cobra.Command{
 		obj := json.ParseObject(r)
 		result := ast.Transform(obj, t)
 		json.Print(result)
+		
+		return nil
 	},
+}
+
+func requireStdin() error {
+	stat, err := os.Stdin.Stat()
+    if err != nil {
+        return fmt.Errorf("failed to stat stdin: %w", err)
+    }
+
+    // If stdin is a terminal, nothing was piped
+    if stat.Mode()&os.ModeCharDevice != 0 {
+        return fmt.Errorf("no input provided on stdin")
+    }
+
+    return nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
---
+++ tests/int_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+
+var cliPath string = "gq"
+
+func TestMain(m *testing.M) {
+    tmp := os.TempDir()
+    cliPath = filepath.Join(tmp, "mycli-test")
+
+    cmd := exec.Command("go", "build", "-o", cliPath, "../cmd/gq")
+    if err := cmd.Run(); err != nil {
+        panic(err)
+    }
+
+    code := m.Run()
+    os.Remove(cliPath)
+    os.Exit(code)
+}
+
+func TestCLI_Version(t *testing.T) {
+    cmd := exec.Command(cliPath, "--version")
+
+    var stdout, stderr bytes.Buffer
+    cmd.Stdout = &stdout
+    cmd.Stderr = &stderr
+
+    err := cmd.Run()
+
+    if err != nil {
+        t.Fatalf("command failed: %v\nstderr: %s", err, stderr.String())
+    }
+
+    if !strings.Contains(stdout.String(), "Version:") {
+        t.Fatalf("unexpected output: %s", stdout.String())
+    }
+}
+
+func TestCLI_Root(t *testing.T) {
+	testCases := []struct {
+		desc, stdin, program, wantOut, wantErr string
+	}{
+		{
+			desc: "root without stdin returns usage",
+			stdin: "",
+			program: "",
+			wantOut: "",
+			wantErr: "Error",
+		},
+		{
+			desc: "root without stdin returns usage",
+			stdin: `{"a": "b"}`,
+			program: "",
+			wantOut: "",
+			wantErr: "no program provided",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			var cmd *exec.Cmd
+			if tC.program == "" {
+				cmd = exec.Command(cliPath)
+			} else {
+				cmd = exec.Command(cliPath, tC.program)
+			}
+
+			if tC.stdin != "" {
+				cmd.Stdin = bytes.NewBuffer([]byte(tC.stdin))
+			}
+
+			var stdout, stderr bytes.Buffer
+			cmd.Stdout = &stdout
+			cmd.Stderr = &stderr
+
+			err := cmd.Run()
+			if err != nil {
+				got := stderr.String()
+				if !strings.Contains(got, tC.wantErr) {
+					t.Fatalf("err: %s\nunexpected stderr output:\ngot: %s\nwanted: %s\n", err, got, tC.wantErr)
+				}
+			} else {
+				got := stdout.String()
+				if !strings.Contains(got, tC.wantOut) {
+					t.Fatalf("unexpected output:\ngot:%s\nwanted:%s\n", got, tC.wantOut)
+				}
+			}
+		})
+	}
+}
