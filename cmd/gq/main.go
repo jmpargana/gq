ac3c02f2ba6d64cba69ba10ef500eda54a93b6ca
Joao Pargana <jmpargana@gmail.com>
2026-01-28 16:48:57 +0000

    feat: add docs generator

--- cmd/gq/main.go
+++ cmd/gq/main.go
@@ -4,99 +4,16 @@ Copyright © 2026 Joao Pargana jmpargana@gmail.com
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 
-	"github.com/jmpargana/gq/internal/ast"
-	json "github.com/jmpargana/gq/internal/gqjson"
-	"github.com/jmpargana/gq/internal/lexer"
-	"github.com/jmpargana/gq/internal/parser"
-	u "github.com/jmpargana/gq/internal/utils"
-	"github.com/spf13/cobra"
+	"github.com/jmpargana/gq/internal/cmd"
 )
 
-var version = "dev"
-
-var rootCmd = &cobra.Command{
-	Use:   "gq",
-	Short: "jq-like command-line tool written in go",
-	Long:  `A fast, simple, and expressive way to query and transform JSON data from the command line`,
-	RunE: func(cmd *cobra.Command, _ []string) error {
-		showVersion, _ := cmd.Flags().GetBool("version")
-		if showVersion {
-			fmt.Printf("Version: %s\n", version)
-			os.Exit(0)
-		}
-
-		if err := requireStdin(); err != nil {
-			return err
-		}
-
-		if len(os.Args) < 2 {
-			return fmt.Errorf("no program provided")
-		}
-
-		r := bufio.NewReader(os.Stdin)
-		// TODO: replace with cobra args
-		tokens := lexer.Lex(os.Args[1])
-		p := parser.NewParser(tokens)
-
-		t := p.ParseExpr()
-
-		debug, _ := cmd.Flags().GetBool("debug")
-		if debug {
-			fmt.Println("Generated AST:")
-			fmt.Println()
-			fmt.Println(ast.PrintAST(t, 0))
-		}
-
-		obj := json.ParseObject(r)
-		stream := u.NewStream()
-		stream.O = append(stream.O, obj)
-		result := ast.TransformStream(stream, t)
-		for _, r := range result.O {
-			json.Print(r)
-		}
-
-		return nil
-	},
-}
-
-func requireStdin() error {
-	stat, err := os.Stdin.Stat()
-	if err != nil {
-		return fmt.Errorf("failed to stat stdin: %w", err)
-	}
-
-	// If stdin is a terminal, nothing was piped
-	if stat.Mode()&os.ModeCharDevice != 0 {
-		return fmt.Errorf("no input provided on stdin")
-	}
-
-	return nil
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := cmd.RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tt.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("version", "v", false, "Display CLI version")
-	rootCmd.Flags().BoolP("debug", "d", false, "Displays AST from requested expression")
 }
 
 func main() {
---
+++ internal/cmd/root.go
@@ -0,0 +1,86 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/jmpargana/gq/internal/ast"
+	json "github.com/jmpargana/gq/internal/gqjson"
+	"github.com/jmpargana/gq/internal/lexer"
+	"github.com/jmpargana/gq/internal/parser"
+	u "github.com/jmpargana/gq/internal/utils"
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "gq",
+	Short: "jq-like command-line tool written in go",
+	Long: `A fast, simple, and expressive way to query 
+and transform JSON data from the command line.
+
+Most of the jqlang syntax (https://jqlang.org/manual/) should be available.
+List of battle tested features:
+	- indexing numbers, strings, identifiers
+	- indexing iterators (.[])
+	- cartesian product of iterators
+	- chaing indexes
+	- array creation
+	- dictionary creation
+	- nested piping
+	
+Additionally, you can also view the AST of your jqlang expression.
+`,
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		if err := requireStdin(); err != nil {
+			return err
+		}
+
+		if len(os.Args) < 2 {
+			return fmt.Errorf("no program provided")
+		}
+
+		r := bufio.NewReader(os.Stdin)
+
+		// TODO: replace with cobra args
+		tokens := lexer.Lex(os.Args[1])
+		p := parser.NewParser(tokens)
+
+		t := p.ParseExpr()
+
+		debug, _ := cmd.Flags().GetBool("debug")
+		if debug {
+			fmt.Println("Generated AST:")
+			fmt.Println()
+			fmt.Println(ast.PrintAST(t, 0))
+		}
+
+		obj := json.ParseObject(r)
+		stream := u.NewStream()
+		stream.O = append(stream.O, obj)
+		result := ast.TransformStream(stream, t)
+		for _, r := range result.O {
+			json.Print(r)
+		}
+
+		return nil
+	},
+}
+
+func requireStdin() error {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to stat stdin: %w", err)
+	}
+
+	// If stdin is a terminal, nothing was piped
+	if stat.Mode()&os.ModeCharDevice != 0 {
+		return fmt.Errorf("no input provided on stdin")
+	}
+
+	return nil
+}
+
+func init() {
+	RootCmd.Flags().BoolP("debug", "d", false, "Displays AST from requested expression")
+}
