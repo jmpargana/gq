d8467241c99b5df72c64b75684514e9c9bd27d3b
Joao Pargana <jmpargana@gmail.com>
2026-01-28 09:07:14 +0000

    feat: add coverage and badges

--- internal/utils/types.go
+++ internal/utils/types.go
@@ -42,35 +42,3 @@ func NewStream() Stream {
 		O: []any{},
 	}
 }
-
-func (c Cmd) IsEqual(o Cmd) bool {
-	if c.Kind != o.Kind {
-		return false
-	}
-	if c.Kind == IDX {
-		if len(c.Fields) != len(o.Fields) {
-			return false
-		}
-
-		for i := range c.Fields {
-			if c.Fields[i] != o.Fields[i] {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func (n Node) IsEqual(o Node) bool {
-	if !n.Value.IsEqual(o.Value) || len(n.Children) != len(o.Children) {
-		return false
-	}
-	if len(n.Children) > 0 {
-		for i := range n.Children {
-			if !n.Children[i].IsEqual(o.Children[i]) {
-				return false
-			}
-		}
-	}
-	return true
-}
